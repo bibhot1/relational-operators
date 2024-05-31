9493388cc90efe35f43ab798afb7acc111fb26ef
Bibhot1 <bndazbvu.me12@gmail.com>
2024-06-01 06:40:59 +1200

    Add project files.

---
+++ relational operators/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num1 = 5;
+int num2 = 6;   
+
+
+// < is less than
+// <= is less than equal
+// > is greater than
+// >= is greater than equal
+
+bool isHigher = num1 > num2;
+
+int age = 12;
+if (age > 18)
+{
+    Console.WriteLine("party");
+}
+else
+{
+    Console.WriteLine("stay at home");
+}
